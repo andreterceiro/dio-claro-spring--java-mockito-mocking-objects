56414cb4ca674c03c74a09f1072fa2fcf5ddaa25
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-27 10:47:46 -0300

    feat: renaming - packacke => pkg

--- arrtifactId/src/main/java/pkg/App.java
+++ arrtifactId/src/main/java/pkg/App.java
@@ -1,3 +1,5 @@
+package pkg;
+
 /**
  * Hello world!
  */
--- arrtifactId/src/test/java/pkg/AppTest.java
+++ arrtifactId/src/test/java/pkg/AppTest.java
@@ -1,5 +1,6 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
+package pkg;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 /**
