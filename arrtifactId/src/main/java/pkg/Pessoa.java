a3455ddf9c41e1943c2a88c8b058321e11653f1d
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-27 10:58:54 -0300

    feat: Pessoa class

---
+++ arrtifactId/src/main/java/pkg/Pessoa.java
@@ -0,0 +1,49 @@
+package pkg;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String nome;
+    private String documento;
+    private LocalDate nascimento;
+    private DadosLocalizacao endereco;
+
+    public Pessoa(String documento, DadosLocalizacao endereco, LocalDate nascimento, String nome) {
+        this.documento = documento;
+        this.endereco = endereco;
+        this.nascimento = nascimento;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public DadosLocalizacao getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(DadosLocalizacao endereco) {
+        this.endereco = endereco;
+    }
+}
