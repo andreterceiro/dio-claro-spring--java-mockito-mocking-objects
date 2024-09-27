a71bb668cfe526672c0544132ac14885edb68a59
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-27 13:09:09 -0300

    test: using the Log class

--- arrtifactId/src/main/java/pkg/CadastroPessoa.java
+++ arrtifactId/src/main/java/pkg/CadastroPessoa.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class CadastroPessoa {
     private final ApiDosCorreios apiDosCorreios;
+    private Log log;
 
     public CadastroPessoa(final ApiDosCorreios apiDosCorreios) {
         this.apiDosCorreios = apiDosCorreios;
@@ -13,6 +14,13 @@ public class CadastroPessoa {
         Pessoa pessoa = new Pessoa(documento, nascimento, nome);
         DadosLocalizacao dadosLocalizacao = apiDosCorreios.buscarDadosComBaseNoCep(cep);
         pessoa.adicionarDadosEndereco(dadosLocalizacao);
+        if (this.log != null) {
+            this.log.log("test");
+        }
         return pessoa;
+    }
+
+    public void adicionarLogger(Log log) {
+        this.log = log;
     }
 }
