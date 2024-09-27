64318521d6927a3377f21fa66cbcc044bfa9ad97
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-27 11:21:13 -0300

    Creating a class ApiDosCorreios - the fact of the method 'buscarDadosComBaseNoCep' return a hardcorded data is not important

---
+++ arrtifactId/src/main/java/pkg/ApiDosCorreios.java
@@ -0,0 +1,9 @@
+package pkg;
+
+public class ApiDosCorreios {
+
+    DadosLocalizacao buscarDadosComBaseNoCep(String cep) {
+        return new DadosLocalizacao("03176001", "São Paulo", "", "Rua Tobias Barreto", "SP");
+    }
+
+}
