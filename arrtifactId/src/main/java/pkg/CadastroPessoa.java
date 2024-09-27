863190784a2f6e9a2fdb0d94484a14c14e72b6a1
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-27 11:21:44 -0300

    feat: CadastroPessoa class

---
+++ arrtifactId/src/main/java/pkg/CadastroPessoa.java
@@ -0,0 +1,18 @@
+package pkg;
+
+import java.time.LocalDate;
+
+public class CadastroPessoa {
+    private ApiDosCorreios apiDosCorreios;
+
+    public CadastroPessoa(final ApiDosCorreios apiDosCorreios) {
+        this.apiDosCorreios = apiDosCorreios;
+    }
+
+    public Pessoa cadastrarPessoa(String nome, String documento, LocalDate nascimento, String cep) {
+        Pessoa pessoa = new Pessoa(documento, nascimento, nome);
+        DadosLocalizacao dadosLocalizacao = apiDosCorreios.buscarDadosComBaseNoCep(cep);
+        pessoa.adicionarDadosEndereco(dadosLocalizacao);
+        return pessoa;
+    }
+}
