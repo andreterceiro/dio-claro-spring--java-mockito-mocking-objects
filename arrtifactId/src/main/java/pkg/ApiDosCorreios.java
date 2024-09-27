5eee1be01607e16dc46e896f5ff22c0aec0319ec
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-27 11:23:29 -0300

    Removing unecessary empty lines

--- arrtifactId/src/main/java/pkg/ApiDosCorreios.java
+++ arrtifactId/src/main/java/pkg/ApiDosCorreios.java
@@ -5,5 +5,4 @@ public class ApiDosCorreios {
     DadosLocalizacao buscarDadosComBaseNoCep(String cep) {
         return new DadosLocalizacao("03176001", "São Paulo", "", "Rua Tobias Barreto", "SP");
     }
-
 }
